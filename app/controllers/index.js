d736089b9f43936180feb5d822d366c6fe80071c
Jared Murphy <jaredcmurphy@gmail.com>
2017-05-04 20:01:27 -0400

    reading and writing with rails api

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,28 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  //isDisabled: true,
-  url: '',
+  url: 'asdf',
+  shortUrl: '',
+  responseMessage: '',
   isValid: Ember.computed.match('url', /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/),
   isDisabled: Ember.computed.not('isValid'),
   topUrls: [],
 
   actions: {
-    activate(){
-      console.log('inside activate')
-      const apiUrl = "http://localhost:300/api/v1/tops";
+    getTopUrls(){
+      const apiUrl = "http://localhost:3000/api/v1/tops";
       Ember.$.ajax({
         url: apiUrl,
         method: "GET"
       }).then(response => {
-        debugger;
-        this.set('topUrls', response)
+        this.set('topUrls', response.urls)
       });
     },
     createShortLink(){
       const url = this.get('url');
-      console.log('we will be back shortly with your shorter version of ', url);
-      this.set('url', '');
+      const apiUrl = "http://localhost:3000/api/v1/urls";
+
+      Ember.$.ajax({
+        url: apiUrl,
+        method: "POST",
+        data: {url: {full_link: url}}
+      }).then(response => {
+        this.send('getTopUrls')
+        const short = response.url.short_link
+        this.set('shortUrl', response.url.short_link)
+        this.set('url', '')
+        this.set('responseMessage', `Thanks! Here is your link: ${this.get('shortUrl')}`)
+      });
     }
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupController(controller, model) {
     $.getJSON('http://localhost:3000/api/v1/tops', data =>{
-      console.log('setting isChecked to: ', data.urls);
       controller.set('topUrls', data.urls);
     })
   }
